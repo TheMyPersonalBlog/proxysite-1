dc2713d6a201a44a3ca2df28b6c277d5237b6d74
NathanBrodrick <163065342+Iambrucea@users.noreply.github.com>
2025-01-31 09:09:15 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.coolmathgames.com/0-planet-clicker/play";//your website url
+  let target = "https://sandstrike.io/;//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
